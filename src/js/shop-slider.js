c0b8539d06263ad2176258bf78f3977e6abea86f
NJul <nina.dev.web@gmail.com>
2019-04-13 18:14:44 +0300

    Shop slider arrows

--- src/js/shop-slider.js
+++ src/js/shop-slider.js
@@ -6,12 +6,23 @@ $(document).ready(function () {
     /* Показывается 3 слайда за 1 раз */
     items: 3,
     /* Зацикливание, бесконечная прокрутка слайдов */
-    // loop: true,
+    loop: true,
     /* Точки выбора слайда по умолчанию true */
     dots: false,
     /* Скорость прокрутки слайда, 1000 = 1 секунда */
-    smartSpeed: 500
+    smartSpeed: 500,
+    /* Отступ между слайдами */
+    margin: 2
   });
+
+  // Go to the next item
+  $('#shopSliderRight').click(function () {
+    shopSlider.trigger('next.owl.carousel');
+  })
+  // Go to the previous item
+  $('#shopSliderLeft').click(function () {
+    shopSlider.trigger('prev.owl.carousel');
+  })
 
 
 

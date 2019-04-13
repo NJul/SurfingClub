c9280930ae923dda3e81d703e3a666f195a90ab3
NJul <nina.dev.web@gmail.com>
2019-04-13 07:59:06 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,45 @@
 "use strict";
 
-console.log(2 + 2);
+$(document).ready(function () {
+  /* В переменную записывается селектор, который выбирает обертку нашего слайдера. Чтобы поттом к этой переменной можно было обращаться. */
+  // $(".owl-carousel").owlCarousel({
+
+  /* Показывается 1 слайд за 1 раз */
+  // items: 1,
+
+  /* Зацикливание, бесконечная прокрутка слайдов */
+  // loop: true,
+
+  /* Точки выбора слайда по умолчанию true */
+  // dots: false,
+
+  /* Скорость прокрутки слайда, 1000 = 1 секунда */
+  // smartSpeed: 1000
+  // });
+  var headerSlider = $(".owl-carousel");
+  headerSlider.owlCarousel({
+    /* Показывается 1 слайд за 1 раз */
+    items: 1,
+
+    /* Зацикливание, бесконечная прокрутка слайдов */
+    loop: true,
+
+    /* Точки выбора слайда по умолчанию true */
+    dots: false,
+
+    /* Скорость прокрутки слайда, 1000 = 1 секунда */
+    smartSpeed: 1000
+  }); // Go to the next item
+
+  /* Находим на странице элемент с контроллом и по клику будем, например, переключать на следующий слайд.  */
+
+  $('#headerSliderRight').click(function () {
+    headerSlider.trigger('next.owl.carousel');
+  }); // Go to the previous item
+
+  $('#headerSliderLeft').click(function () {
+    // With optional speed parameter
+    // Parameters has to be in square bracket , '[]' )
+    headerSlider.trigger('prev.owl.carousel');
+  });
+});
